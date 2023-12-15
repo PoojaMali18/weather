592cd8e766cd2ce6ee2268a883aff36944fb1535
PoojaMali18 <mpooja9950@gmail.com>
2023-12-16 01:39:11 +0530

    Weather App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+//import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [latitude, setlatitude] = useState(0);
+    const [longitude, setlongitude] = useState(0);
+    const [hemisphere, sethemisphere] = useState("");
+    const [month, setmonth] = useState(() => { return new Date().getMonth() + 1 })
+    function fetdhlocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    setlatitude(position.coords.latitude)
+                    setlongitude(position.coords.longitude)
+                    if (position.coords.latitude > 0) {
+                        sethemisphere("Northen hemisphere");
+                    }
+                    else if (position.coords.latitude < 0) {
+                        sethemisphere("southern henisphere");
+                    }
+                    else {
+                        sethemisphere("Equator");
+                    }
+                }
+            )
+        }
+        else {
+
+        }
+    }
+    return (
+        <div>
+            <button onClick={fetdhlocation}> Fetch Location</button>
+            <p>Latitide :{latitude}</p>
+            <p>longitude :{longitude}</p>
+            <p>hemisphere:{hemisphere}</p>
+            <p>Month:{month}</p>
+            {
+                hemisphere && (hemisphere == "Northen Hemisphere" && month >= 4 && month <= 10) &&
+                (
+                    <div>
+                        <img src="https://th.bing.com/th/id/OIP.TAOCnNsrBermZeh9jq6nnQHaEo?rs=1&pid=ImgDetMain"></img>
+                    </div>
+                )
+            }
+            {
+                hemisphere && (hemisphere == "Northen Hemisphere" && month<4  || month>10) &&
+                (
+                    <div>
+                        <img src="https://th.bing.com/th/id/OIP.jmo1bagfwD0cdtSIGrvQVwHaE1?rs=1&pid=ImgDetMain"></img>
+                    </div>
+                )
+            }
+        </div>
+    )
 }
 
 export default App;
